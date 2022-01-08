818937236c174e88d207af43fa9507f54eee094f
renangton <puccho0427@gmail.com>
2022-01-09 05:29:05 +0900

    初コミット

---
+++ Main.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Hello Worldを出力
+		System.out.println("Hello World");
+				
+		for(int i = 1; i <= 20; i++) {
+			// 10回目まではHello Worldを出力
+			if(i <= 10) {
+				System.out.println("Hello World");
+			}else if(i == 20) {
+				System.out.println("Good Bye");
+			//　11回目からはHelloのみ出力
+			}else {
+				System.out.println("Hello");
+			}
+		}
+	}
+}
